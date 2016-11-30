0459c742a155c3a91e86c0404647edc0b2d32f97
j-west <westja89@gmail.com>
2016-11-29 21:18:47 -0600

    Delete two console logs

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 var num1 = (1 * 1) + (2 * 2) + (3 * 3) + (4 * 4) + (5 * 5) + (6 * 6) + (7 * 7) + (8 * 8) + (9 * 9) + (10 * 10);
 var num2 = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
 num2 *= num2;
-console.log(num1);
-console.log(num2);
 console.log("The difference is " + (num2 - num1));
